b79c1a191d7e22519324a948b57cda505e8e6f50
qibill <870937917@qq.com>
2018-06-05 23:12:43 +0800

    1.完成log4j 的日志输出

--- asmxWebservice/src/main/java/com/biosan/webservice/BiosanWebSerivceImpl.java
+++ asmxWebservice/src/main/java/com/biosan/webservice/BiosanWebSerivceImpl.java
@@ -1,13 +1,17 @@
 package com.biosan.webservice;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.biosan.utils.BiosanResult;
 import com.biosan.utils.JsonUtils;
 import com.biosan.utils.RequestUtil;
 import com.newtouch.pojo.PatientDetailInfoRequest;
 
 public class BiosanWebSerivceImpl implements BiosanWebSerivce{
-
+    
+    private static final Logger logger = Logger.getLogger(BiosanWebSerivceImpl.class);
+    
     @Autowired
 	private NewTouchService newtouchservice;
 /*    @Autowired
@@ -15,9 +19,15 @@ public class BiosanWebSerivceImpl implements BiosanWebSerivce{
 	
 	@Override
 	public String getPatientDetailInfo(String CardNo) {
-
+	    logger.info("开始查找就诊卡号为" + CardNo + "病人基本信息(唐筛)");
 		PatientDetailInfoRequest request = new PatientDetailInfoRequest(CardNo);
 		String patientDetailInfo = newtouchservice.patientDetailInfo(request.toXml());
+		BiosanResult biosanResult = RequestUtil.getPatientDetailInfoRequest(patientDetailInfo);
+		if (biosanResult.getStatus() == 1) {
+		    logger.info("查询成功");
+        }else{
+            logger.info("查询失败，失败信息：" +  biosanResult.getMsg());
+        };
 		return JsonUtils.objectToJson(RequestUtil.getPatientDetailInfoRequest(patientDetailInfo));
 	}
 
--- asmxWebservice/src/main/java/com/biosan/webservice/biosanJob.java
+++ asmxWebservice/src/main/java/com/biosan/webservice/biosanJob.java
@@ -2,6 +2,7 @@ package com.biosan.webservice;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -18,60 +19,77 @@ import com.newtouch.pojo.PlatFormTSSCServiceRequest;
 @Component
 public class biosanJob {
 
-	@Autowired
-	private TSSCService tSSCSrevice;
-	@Autowired
-	private NewTouchService newtouchservice;
-	@Autowired
-	private NewtouchtsscresultService newtouchtsscresultService;
+    private static final Logger logger = Logger.getLogger(biosanJob.class);
 
-	private Integer days = 1;
+    @Autowired
+    private TSSCService tSSCSrevice;
 
-	public void execute() {
-		BiosanResult result = new BiosanResult();
-		List<Newtouchtsscresult> list = newtouchtsscresultService.selectDaysSampleid(days);
-		for (Newtouchtsscresult newtouchtsscresult : list) {
-			// 未发过 表newtouchtsscresult没有数据
-			if (newtouchtsscresult.getTsscresult() == null) {
-				result = sendPlatFormTSSCService(newtouchtsscresult.getSampleid(), 1);
-				newtouchtsscresult.setTsscresult(result.getStatus());
-				newtouchtsscresultService.insert(newtouchtsscresult);
-			} else {
-				// 表newtouchtsscresult有数据
-				// 更新  pdfdate报告日期
-				// 成功发送过
-				if (newtouchtsscresult.getTsscresult() == 1) {
-					result = sendPlatFormTSSCService(newtouchtsscresult.getSampleid(), 3);
-					if (result.getStatus() == 1) {
-						result = sendPlatFormTSSCService(newtouchtsscresult.getSampleid(), 1);
-					}
-					newtouchtsscresult.setTsscresult(result.getStatus());
-					newtouchtsscresultService.updateByBean(newtouchtsscresult);
-				} else {
-					// 发送失败的 pdfdate报告日期在一天之内
-					result = sendPlatFormTSSCService(newtouchtsscresult.getSampleid(), 1);
-					newtouchtsscresult.setTsscresult(result.getStatus());
-					newtouchtsscresultService.updateByBean(newtouchtsscresult);
-				}
-			}
-		}
-		// 曾经发送失败的  pdfdate报告日期 包括在一天之前
-		List<Newtouchtsscresult> Errlist = newtouchtsscresultService.selectErr();
-		for (Newtouchtsscresult newtouchtsscresult : Errlist) {
-			result = sendPlatFormTSSCService(newtouchtsscresult.getSampleid(), 1);
-			newtouchtsscresult.setTsscresult(result.getStatus());
-			newtouchtsscresultService.updateByBean(newtouchtsscresult);
-		}
-	}
+    @Autowired
+    private NewTouchService newtouchservice;
 
-	public BiosanResult sendPlatFormTSSCService(int sampleid, Integer czqf) {
-		
-		PlatFormTSSCServiceRequest request = tSSCSrevice.creator(sampleid, czqf);
-		String tSSCReponse = newtouchservice.PlatForm_TSSC_Service(request.toXml());
-		// 运行时有异常
-		if (tSSCReponse == null) {
-			return BiosanResult.isErr();
-		}
-		return RequestUtil.getTSSCRequest(tSSCReponse);
-	}
+    @Autowired
+    private NewtouchtsscresultService newtouchtsscresultService;
+
+    private Integer days = 1;
+
+    public void execute() {
+        logger.info("开始发送病人唐氏筛查信息");
+        BiosanResult result = new BiosanResult();
+        List<Newtouchtsscresult> list = newtouchtsscresultService.selectDaysSampleid(days);
+        logger.info("有" + list.size() + "个新报告信息");
+        for (Newtouchtsscresult newtouchtsscresult : list) {
+            // 未发过 表newtouchtsscresult没有数据
+            if (newtouchtsscresult.getTsscresult() == null) {
+                result = sendPlatFormTSSCService(newtouchtsscresult.getSampleid(), 1);
+                newtouchtsscresult.setTsscresult(result.getStatus());
+                newtouchtsscresultService.insert(newtouchtsscresult);
+            }
+            else {
+                // 表newtouchtsscresult有数据
+                // 更新 pdfdate报告日期
+                // 成功发送过
+                logger.info("样品ID为" + newtouchtsscresult.getSampleid() + "重新风险计算了，发送撤销命令");
+                if (newtouchtsscresult.getTsscresult() == 1) {
+                    result = sendPlatFormTSSCService(newtouchtsscresult.getSampleid(), 3);
+                    if (result.getStatus() == 1) {
+                        result = sendPlatFormTSSCService(newtouchtsscresult.getSampleid(), 1);
+                    }
+                    newtouchtsscresult.setTsscresult(result.getStatus());
+                    newtouchtsscresultService.updateByBean(newtouchtsscresult);
+                }
+                else {
+                    // 发送失败的 pdfdate报告日期在一天之内
+                    result = sendPlatFormTSSCService(newtouchtsscresult.getSampleid(), 1);
+
+                    newtouchtsscresult.setTsscresult(result.getStatus());
+                    newtouchtsscresultService.updateByBean(newtouchtsscresult);
+                }
+            }
+        }
+        // 曾经发送失败的 pdfdate报告日期 包括在一天之前
+        List<Newtouchtsscresult> Errlist = newtouchtsscresultService.selectErr();
+        logger.info("有" + list.size() + "发送失败的报告信息");
+        for (Newtouchtsscresult newtouchtsscresult : Errlist) {
+            result = sendPlatFormTSSCService(newtouchtsscresult.getSampleid(), 1);
+            newtouchtsscresult.setTsscresult(result.getStatus());
+            newtouchtsscresultService.updateByBean(newtouchtsscresult);
+        }
+    }
+
+    public BiosanResult sendPlatFormTSSCService(int sampleid, Integer czqf) {
+        String report = czqf == 1 ? "报告信息" : "撤销命令";
+        logger.info("样品ID为" + sampleid + "开始发送" + report);
+        PlatFormTSSCServiceRequest request = tSSCSrevice.creator(sampleid, czqf);
+        String tSSCReponse = newtouchservice.PlatForm_TSSC_Service(request.toXml());
+        // 运行时有异常
+        if (tSSCReponse == null) {
+            logger.info("样品ID为" + sampleid + report + "发送失败");
+            return BiosanResult.isErr();
+        }
+        BiosanResult biosanResult = RequestUtil.getTSSCRequest(tSSCReponse);
+        String result = biosanResult.getStatus() == 1 ? "成功" : "失败";
+        logger.info("样品ID为" + sampleid + report + "发送" + result);
+        return biosanResult;
+    }
+
 }
